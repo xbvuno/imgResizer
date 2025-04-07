008e234054e81cb535ae6b28b59fd67dffdc8f25
xbvuno <cerrabruno.ist@gmail.com>
2025-04-07 22:42:13 +0200

    moved tooltip in PWA mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './css/index.css'
-import './css/bvuno-pico.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./css/index.css";
+import "./css/bvuno-pico.css";
+import App from "./App.jsx";
 
-import { registerSW } from 'virtual:pwa-register'
+import { registerSW } from "virtual:pwa-register";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
 
 const updateSW = registerSW({
-  onNeedRefresh() {
-    console.log('Nuova versione disponibile. Ricarica la pagina.')
-  },
-  onOfflineReady() {
-    console.log('L’app è pronta per l’uso offline.')
-  }
-})
+	onNeedRefresh() {
+		console.log("Nuova versione disponibile. Ricarica la pagina.");
+	},
+	onOfflineReady() {
+		console.log("L’app è pronta per l’uso offline.");
+	},
+});
 
 window.onload = () => {
-  if (window.matchMedia('(display-mode: standalone)').matches) {
-    MoveHeader();
-  }
+	if (window.matchMedia("(display-mode: standalone)").matches) {
+		MoveHeader();
+	}
 };
 
 function MoveHeader() {
-  const root = document.getElementById('root');
-  const header = root.querySelector('header');
-  const main = root.querySelector('main');
+	const root = document.getElementById("root");
+	const header = root.querySelector("header");
+	const main = root.querySelector("main");
 
-  if (header && main) main.parentNode.insertBefore(header, main.nextSibling);
+	if (!header || !main) return;
+
+	main.parentNode.insertBefore(header, main.nextSibling);
+	header.querySelectorAll("#buttons button").forEach((button) => {
+		button.setAttribute("data-placement", "topleft");
+	});
 }
