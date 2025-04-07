a1cc2dd696f4e72dbbb469f86d213c7b4f286c78
xbvuno <cerrabruno.ist@gmail.com>
2025-04-07 10:10:41 +0200

    new ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,45 @@ import { SettingsProvider, SettingsDialogOpener } from "./SettingsContext";
 import Resizer from "./ResizerContext";
 
 function SymbolButton(props) {
-	return (
-		<button className='symbol' {...props}>
-			{props.children}
-		</button>
-	);
+  return (
+    <button className="symbol" {...props}>
+      {props.children}
+    </button>
+  );
 }
 
 function App() {
-	return (
-		<>
-			<SettingsProvider>
-				<header className='container-fluid'>
-					<div className='title'>
-						<img src='media/icons/icon.svg'/>
-						<hgroup>
-							<p>BVUNO'S</p>
-							<h2>imgResizer</h2>
-							<p>v 1.1</p>
-						</hgroup>
-					</div>
-					<div>
-						<SettingsDialogOpener>
-							<SymbolButton>settings</SymbolButton>
-						</SettingsDialogOpener>
-						<a href="https://github.com/xbvuno/imgResizer"><SymbolButton ><img src="media/github.svg"/></SymbolButton></a>
-					</div>
-				</header>
+  return (
+    <>
+      <SettingsProvider>
+        <header className="container-fluid">
+          <div className="title">
+            <img src="media/icons/icon.svg" />
+            <b>imgResizer</b>
+          </div>
+          <div>
+            <a target="_blank" href="https://github.com/xbvuno/imgResizer">
+              <SymbolButton>
+                <img src="media/github.svg" />
+              </SymbolButton>
+            </a>
+            <SymbolButton>info</SymbolButton>
+            <SettingsDialogOpener>
+              <SymbolButton>settings</SymbolButton>
+            </SettingsDialogOpener>
+          </div>
+        </header>
 
-				<main className='container'>
-					<Resizer />
-					<p>all the information provided are not stored anywhere but your browser.</p>
-				</main>
-			</SettingsProvider>
-		</>
-	);
+        <main className="container">
+          <Resizer />
+          <p>
+            ‼️ all the images provided are not stored anywhere but your browser
+            ‼️
+          </p>
+        </main>
+      </SettingsProvider>
+    </>
+  );
 }
 
 export default App;
